02a1a81625c1eeeb7663ee3c9a28ff948e919d3b
Matt Smith <mattonfoot@gmail.com>
2017-01-17 14:01:49 +0000

    initial Key Worker tooling

---
+++ tasks/sass.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+script=public/main.css
+
+cd ./lib
+
+mkdir .tmp
+
+# copy sass to temp location
+cp -fr ./assets/sass/* .tmp/
+cp -fr ./bower_components/bootstrap-sass/assets/stylesheets/* .tmp/
+
+# compile sass
+./node_modules/node-sass/bin/node-sass --output-style expanded .tmp/main.scss > .tmp/main.css
+rm -f "${script}"
+cp .tmp/main.css "${script}"
+
+# clean up
+rm -fr .tmp
+
+# copy additional assets to public folder
+cp -fr ./bower_components/bootstrap-sass/assets/fonts ./public
---
+++ tasks/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+script=./public/main.js
+
+cd ./lib
+
+# move scripts and concat where needed
+cp ./bower_components/jquery/dist/jquery.min.js ./public/jquery.js
+# cp ./bower_components/jquery-ujs ./public/jquery-ujs.js
+cp ./bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js ./public/bootstrap.js
+
+rm -f "${script}"
+cp ./assets/scripts/govuk-admin-template/govuk-admin.js "${script}"
+find ./assets/scripts/govuk-admin-template/modules -type f -name '*.js' -exec cat {} + >> "${script}"
+cat ./assets/scripts/govuk-admin-template.js >> "${script}"
