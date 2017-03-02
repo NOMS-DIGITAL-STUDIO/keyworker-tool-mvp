7a17fd4f19871448b0031754e6e5daad6db620cf
Matt Smith <mattonfoot@gmail.com>
2017-03-02 15:38:09 +0000

    included IE8 polyfils

--- tasks/script.sh
+++ tasks/script.sh
@@ -7,6 +7,9 @@ cp ./bower_components/jquery/dist/jquery.min.js ./public/jquery.js
 # cp ./bower_components/jquery-ujs ./public/jquery-ujs.js
 cp ./bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js ./public/bootstrap.js
 
+cp ./assets/scripts/govuk-admin-template/vendor/html5.js ./public/html5.js
+cp ./assets/scripts/govuk-admin-template/vendor/respond.min.js ./public/respond.min.js
+
 rm -f "${script}"
 cp ./assets/scripts/govuk-admin-template/govuk-admin.js "${script}"
 find ./assets/scripts/govuk-admin-template/modules -type f -name '*.js' -exec cat {} + >> "${script}"
